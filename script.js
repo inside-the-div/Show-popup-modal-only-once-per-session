ea28b0a309ec98261d3d0a459742936ca65271b4
Nasir Khan <nasirkhan.webdev@gmail.com>
2021-09-05 12:33:45 +0600

    just upload main file

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	// show automatically after 5 s
+	setTimeout(showModal,1000);
+	$("#closeBtn").click(function(){
+		$("#myModal").hide()
+	})
+	function showModal(){
+		// get value from localStorage
+		var is_modal_show = sessionStorage.getItem('alreadyShow');
+		if(is_modal_show != 'alredy shown'){
+			$("#myModal").show()
+			sessionStorage.setItem('alreadyShow','alredy shown');
+		}else{
+			console.log(is_modal_show);
+		}
+	}
+})
